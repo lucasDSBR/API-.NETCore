de95027c3e8a19366dc7da0b1612fb8cb94dfeb7
lucas <lucasmaciel6690@gmail.com>
2021-10-26 13:37:04 -0300

    Application e Swegger

--- README.md
+++ README.md
@@ -17,8 +17,38 @@
         -  dotnet add package Microsoft.EntityFrameworkCore.Tools --version 3.1.6
     - Microsoft.EntityFrameworkCore.Design 
         -  dotnet add package Microsoft.EntityFrameworkCore.Design --version 3.1.6
+    - AutoMapper.Extensions.Microsoft.DependencyInjection
+        - dotnet add package AutoMapper.Extensions.Microsoft.DependencyInjection --version 6.1.1
 
 
 ## Criando banco de dados
     - dotnet: dotnet ef migrations add <Nome da migração>
     - dotnet: dotnet ef database update
+
+
+
+## Resultados obitidos por meios das requisições GET, PUT, POST:
+<pre>
+[
+    {
+        "name": "usuario usuario",
+        "email": "usuario2@gmail.com",
+        "instituicao": "instituicao TAL",
+        "tipoUsuario": "Comissão",
+        "id": "0ea81b07-f17f-45d2-b4aa-a4953f76cb8e",
+        "createAt": "2021-10-26T15:06:24.978678",
+        "updateAt": "2021-10-26T15:12:18.377602"
+    },
+    {
+        "name": "usuario1 usuario1",
+        "email": "usuario1@gmail.com",
+        "instituicao": "instituicao TAL",
+        "tipoUsuario": "Comissão",
+        "id": "6176f978-282e-42b7-8599-3f961fdeb490",
+        "createAt": "2021-10-26T14:58:21.517226",
+        "updateAt": "2021-10-26T15:13:39.578876"
+    }
+]
+</pre>
+
+## Swegger:
--- src/Api.Application/Startup.cs
+++ src/Api.Application/Startup.cs
@@ -2,13 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Api.CrossCutting.DependencyInjectable;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace application
 {
@@ -24,7 +27,30 @@ namespace application
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            ConfigureRepository.ConfigureDependenciesRepository(services);
+            ConfigureService.ConfigureDependenciesService(services);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "API com AspNetCore 3.1",
+                    Description = "Arquitetura DDD",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Lucas Silva",
+                        Email = "lucasmaciel6690@mail.com"
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Termo de Licença de Uso",
+                        Url = new Uri("#")
+                    }
+                });
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +60,13 @@ namespace application
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API com AspNetCore 3.1");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseRouting();
 
